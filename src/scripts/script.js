5cb9752696f6918211c000c931d0bb214366f4e8
gabriel-baptista-0607 <gabriel.baptista@ogfigovernance.com>
2025-08-30 22:49:38 -0300

    add: better UI

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -10,10 +10,15 @@ function renderTasks() {
   taskList.textContent = "";
   tasks.forEach((task, index) => {
     const li = document.createElement("li");
+    const i = document.createElement("i");
     li.textContent = task;
     li.setAttribute("data-index", index);
+    li.classList.add("list-item");
+    i.classList.add("ph");
+    i.classList.add("ph-x-circle");
     console.log(index);
     taskList.append(li);
+    li.append(i);
   });
   console.log("Task List Rerendered!");
 }
@@ -30,8 +35,8 @@ form.addEventListener("submit", (e) => {
 
 // remove function
 taskList.addEventListener("click", (e) => {
-  console.log(e.target.closest("li"));
-  const index = Number(e.target.closest("li").getAttribute("data-index"));
+  console.log(e.target.closest("i"));
+  const index = Number(e.target.closest("i").getAttribute("data-index"));
   tasks.splice(index, 1);
   renderTasks();
 });
