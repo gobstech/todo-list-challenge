dffbdb2a05b04210cfb492270ceba68185734793
gabriel-baptista-0607 <gabriel.baptista@ogfigovernance.com>
2025-08-31 11:23:04 -0300

    add: local Storage and category features

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,35 +1,82 @@
 // Get the variables from HTML
 const form = document.querySelector("form");
 const taskList = document.querySelector("#tasks");
-const selectCategory = document.getElementById("categories");
+const select = document.getElementById("categories");
 const categoryForm = document.querySelector(".categoryForm");
+const categoryButton = document.querySelector(".newCategorySubmit");
 
-console.log(categoryForm);
+// Future feature
+// function createCategory() {
+//   const categoryInput = document.getElementById("categoryInput");
+//   console.log(categoryInput.value);
+//   categories.push(categoryInput.value);
+//   categoryInput.value = "";
+//   renderCategories();
+// }
 
-console.log(selectCategory);
+// Create Object
+function createTask(taskName, category) {
+  const task = {};
 
-selectCategory.onchange = (e) => {
-  console.log(e.target.value);
+  task.name = taskName;
+  task.category = category;
 
-  if (e.target.value === "addCategory") {
-    categoryForm.classList.add("active");
-    return;
-  }
-  categoryForm.classList.remove("active");
-};
+  return task;
+}
 
-const tasks = [];
-// const tasks = ["Task 1", "Task 2"];
+const taskOne = createTask("Do Homework", "School");
+
+// Create tasks array
+const tasks = [
+  {
+    name: "Do Homework",
+    category: "School",
+  },
+  {
+    name: "Buy Food",
+    category: "Food",
+  },
+  {
+    name: "Finish Work Task",
+    category: "Work",
+  },
+];
+
+tasks.forEach((task) => {
+  console.log(task.name, task.category);
+});
+
+// Create categories array
+const categories = ["Work", "School", "Home"];
+
 renderTasks();
 
+console.log(tasks);
+
+function renderCategories() {
+  categories.forEach((category) => {
+    console.log(category);
+  });
+}
+
 function renderTasks() {
+  tasks.forEach((task, index) => {
+    localStorage.setItem(`Task ${index + 1}`, JSON.stringify(task));
+  });
   taskList.textContent = "";
   tasks.forEach((task, index) => {
     const li = document.createElement("li");
     const i = document.createElement("i");
-    li.textContent = task;
+    const spanCategory = document.createElement("span");
+    const spanTask = document.createElement("span");
     li.setAttribute("data-index", index);
     li.classList.add("list-item");
+    li.append(spanCategory);
+    spanCategory.classList.add("category");
+    spanCategory.textContent = task.category;
+    li.append(spanTask);
+    spanTask.classList.add("task");
+    spanTask.textContent = task.name;
     i.classList.add("ph");
     i.classList.add("ph-x-circle");
     console.log(index);
@@ -43,7 +90,9 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   const input = document.querySelector("#taskInput");
   console.log(input.value);
-  tasks.unshift(input.value);
+  let category = select.value;
+  let task = new createTask(input.value, category);
+  tasks.push(task);
   console.log(tasks);
   input.value = "";
   renderTasks();
@@ -54,5 +103,6 @@ taskList.addEventListener("click", (e) => {
   console.log(e.target.closest("i"));
   const index = Number(e.target.closest("i").getAttribute("data-index"));
   tasks.splice(index, 1);
+  localStorage.clear();
   renderTasks();
 });
